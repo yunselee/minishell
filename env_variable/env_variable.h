6b6e1e067706cae22f5d55573a98fd0d4569a243
yunselee <42.4.yunselee@gmail.com>
2022-03-18 19:26:59 +0900

    fix, add function env_variable

--- env_variable/env_variable.h
+++ env_variable/env_variable.h
@@ -17,8 +17,10 @@ typedef struct s_env_variable_list
 	int				size;
 }				t_env_variable_list;
 
-const char*	get_env_variable_or_null(const char *key);
-void		register_env_variable(const char *key, const char *value);
-void		remove_env_variable(const char *key);
+const char					*get_env_variable_or_null(const char *key);
+void						register_env_variable
+								(const char *key, const char *value);
+void						remove_env_variable(const char *key);
+const t_env_variable_list	*get_all_env(void);
 
 #endif /* ENV_VARIABLE_H */
