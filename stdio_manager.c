37ba61bc9cf785507dd5bf440fe0136f8919da47
yunselee <42.4.yunselee@gmail.com>
2022-03-24 16:51:47 +0900

    add stdio_manager

---
+++ stdio_manager.c
@@ -0,0 +1,35 @@
+
+#include "allow_function/allow_function.h"
+#include <unistd.h>
+
+static int *get_back_up_fd(void)
+{
+	static int *back_up_fd[2];
+
+	return (back_up_fd);
+}
+
+void stdio_back_up(void)
+{
+	int *back_up_fd;
+
+	back_up_fd = get_back_up_fd();
+	back_up_fd[0] = _dup(STDIN_FILENO);
+	back_up_fd[2] = _dup(STDOUT_FILENO);
+}
+
+void stdio_recover(void)
+{
+	const int *back_up_fd = get_back_up_fd();
+
+	_dup2(back_up_fd[0], STDIN_FILENO);
+	_dup2(back_up_fd[1], STDOUT_FILENO);
+}
+
+void stdio_close_back_up_fd(void)
+{
+	const int *back_up_fd = get_back_up_fd();
+
+	_close(back_up_fd[0]);
+	_close(back_up_fd[1]);
+}
---
+++ stdio_manager.h
@@ -0,0 +1,5 @@
+
+
+void stdio_back_up(void);
+void stdio_recover(void);
+void stdio_close_back_up(void);
