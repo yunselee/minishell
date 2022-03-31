8746477ed4b82bc098fe44005b22277929ecb169
yunselee <42.4.yunselee@gmail.com>
2022-03-31 17:51:01 +0900

    Update sig.h

--- sig/sig.h
+++ sig/sig.h
@@ -6,7 +6,7 @@
 /*   By: yunselee <yunselee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 16:52:22 by yunselee          #+#    #+#             */
-/*   Updated: 2022/03/29 19:15:28 by yunselee         ###   ########.fr       */
+/*   Updated: 2022/03/31 16:12:17 by yunselee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 
 void	sig_set(void);
 void	sig_disable(void);
+void	sig_set_child(void);
 
 #endif
