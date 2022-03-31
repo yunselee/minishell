331ecf2a7cdf784d30a3551040d7e870ed5580a1
Seunghyeon Kim <seunghyk@c2r9s6.42seoul.kr>
2022-03-31 16:17:38 +0900

    Fix built_in_export to be sorted

--- builtin/builtin_export.c
+++ builtin/builtin_export.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_export.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yunselee <yunselee@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: seunghyk <seunghyk@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 17:03:30 by yunselee          #+#    #+#             */
-/*   Updated: 2022/03/29 19:36:31 by yunselee         ###   ########.fr       */
+/*   Updated: 2022/03/31 16:16:03 by seunghyk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,62 @@
 #include "../env_variable/env_variable.h"
 #include "../libft/libft.h"
 #include <stdio.h>
+
+static int	partition(char **envs, int left, int right)
+{
+	char	*pivotKey;
+	int		pivotPos;
+	int		i;
+	char	*temp;
+
+	pivotKey = envs[right];
+	pivotPos = left;
+	i = left;
+	while (i < right)
+	{
+		if (0 < ft_strcmp(pivotKey, envs[i]))
+		{
+			temp = envs[pivotPos];
+			envs[pivotPos] = envs[i];
+			envs[i] = temp;
+			++pivotPos;
+		}
+		++i;
+	}
+	return (pivotPos);
+}
+
+static void	sort_recursive(char **envs, int left, int right)
+{
+	char	*temp;
+	int		pivot;
+
+	if (right <= left)
+	{
+		return ;
+	}
+	pivot = partition(envs, left, right);
+	temp = envs[pivot];
+	envs[pivot] = envs[right];
+	envs[right] = temp;
+	sort_recursive(envs, left, pivot - 1);
+	sort_recursive(envs, pivot + 1, right);
+}
+
+static void	sort(char **envs)
+{
+	char	**p_envs;
+	int		n;
+
+	p_envs = envs;
+	n = 0;
+	while (*p_envs != NULL)
+	{
+		++n;
+		++p_envs;
+	}
+	sort_recursive(envs, 0, n - 1);
+}
 
 static char	*tokenize(char *str_or_null, const char *delims)
 {
@@ -45,18 +101,19 @@ void	builtin_export(char **args)
 	char	*key;
 	char	*value;
 	int		i;
-	char	**env;
+	char	**envs;
 
 	if (args[1] == NULL)
 	{
 		i = 0;
-		env = get_all_env_malloc();
-		while (env[i] != NULL)
+		envs = get_all_env_malloc();
+		sort(envs);
+		while (envs[i] != NULL)
 		{
-			printf("declare -x %s\n", env[i]);
+			printf("declare -x %s\n", envs[i]);
 			i++;
 		}
-		destroy_envs(env);
+		destroy_envs(envs);
 	}
 	i = 1;
 	while (args[i] != NULL)
