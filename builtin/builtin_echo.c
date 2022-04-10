b040dcb56b4f1bfc3149cff488e25b0814bdca91
yunselee <42.4.yunselee@gmail.com>
2022-04-10 18:10:11 +0900

    fix echo args

--- builtin/builtin_echo.c
+++ builtin/builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: yunselee <yunselee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 17:04:06 by yunselee          #+#    #+#             */
-/*   Updated: 2022/04/05 19:08:58 by yunselee         ###   ########.fr       */
+/*   Updated: 2022/04/10 18:09:25 by yunselee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,19 +37,25 @@ void	builtin_echo(const char **args)
 {
 	bool	is_newline;
 	int		i;
+	bool	is_arg;
 
 	i = 1;
 	is_newline = true;
-	while (args[i] != NULL && args[i][0] == '-')
+	is_arg = true;
+	while (is_arg && args[i] != NULL && args[i][0] == '-')
 	{
 		if (in_arg(args[i], ARG))
+		{
 			is_newline = false;
-		i++;
+			i++;
+		}
+		else
+			is_arg = false;
 	}
-	while (args[i] != NULL)
+	while (args[i] != NULL && printf("%s", args[i]) && i++)
 	{
-		printf("%s ", args[i]);
-		i++;
+		if (args[i] != NULL)
+			printf(" ");
 	}
 	if (is_newline)
 		printf("\n");
